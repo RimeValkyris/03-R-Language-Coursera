2f1e8ebf4e60cc5488010f82f429f52d5c9f29f5
Derpentolongist <62804814+Derpentolongist@users.noreply.github.com>
2025-09-27 19:37:10 +0800

    Vectors and List

---
+++ Vectors and List/Vectors_and_List.R
@@ -0,0 +1,17 @@
+# Vectors and Lists in R
+#Example of creating a numeric vector using the c() function
+c(2.5, 48.5, 101.5)
+
+#To create a vector of integers with the c() function, you must place the letter L directly after each number.
+c(1L, 2L, 3L)
+
+
+#You can also create a vector containing characters or logicals. 
+c("Alpha", "Beta", "Gamma")
+c(TRUE, FALSE, TRUE, TRUE)
+
+#To create a vector of a sequence of numbers, use a colon to separate the starting number and the ending number. For example, the following code creates a vector, z, that includes the whole numbers from 4 through 10. Run the code block to display the vector.
+z <- c(4:10)
+z
+
+##Determine vector properties
